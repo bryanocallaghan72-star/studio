b128dcc405a2d032d57ce88763b410e5bc10d199
Bryan O'Callaghan <bryanocallaghan72@gmail.com>
2025-10-13 13:01:26 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8946896495-11b4f",
+  "appId": "1:863885862509:web:2b5a426d863bda90d88164",
+  "apiKey": "AIzaSyC4ptOXoPUF8pnUHlRAUE4e-It2-7p4HY0",
+  "authDomain": "studio-8946896495-11b4f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "863885862509"
+};
