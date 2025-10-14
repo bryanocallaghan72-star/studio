4459a507e5216650d1b97c930aa9cf3948927c7d
Bryan O'Callaghan <bryanocallaghan72@gmail.com>
2025-10-14 12:15:44 +0000

    its a good idea but i was thinking having that as page of its own.. the

--- src/app/stays-deals/page.tsx
+++ src/app/stays-deals/page.tsx
@@ -1,14 +1,14 @@
 
 import { Header } from "@/components/iykyk/Header";
-import { StaysList } from "@/components/iykyk/StaysList";
+import { StaysDeals } from "@/components/iykyk/StaysDeals";
 import { MobileNav } from "@/components/iykyk/MobileNav";
 
-export default function StaysPage() {
+export default function StaysDealsPage() {
   return (
     <div className="flex min-h-screen w-full flex-col bg-background">
       <Header />
       <main className="flex-1 p-4 md:p-6 pb-24">
-        <StaysList />
+        <StaysDeals />
       </main>
       <MobileNav />
     </div>
