853f9815d1e8496111c08aa63b672865d801a525
Bryan O'Callaghan <bryanocallaghan72@gmail.com>
2025-10-16 03:32:52 +0000

    anything else iykyk fire/deals

--- src/app/flash-stays/page.tsx
+++ src/app/flash-stays/page.tsx
@@ -1,6 +1,6 @@
 
 import { Header } from "@/components/iykyk/Header";
-import { StaysDeals } from "@/components/iykyk/StaysDeals";
+import { HotNow } from "@/components/iykyk/HotNow";
 import { MobileNav } from "@/components/iykyk/MobileNav";
 
 export default function FlashStaysPage() {
@@ -8,7 +8,7 @@ export default function FlashStaysPage() {
     <div className="flex min-h-screen w-full flex-col bg-background">
       <Header />
       <main className="flex-1 p-4 md:p-6 pb-24">
-        <StaysDeals />
+        <HotNow />
       </main>
       <MobileNav />
     </div>
