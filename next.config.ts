56f6a1121000ff7751b2d956441c9aabdbc0e094
Bryan O'Callaghan <bryanocallaghan72@gmail.com>
2025-10-20 02:53:48 +0000

    Try fixing this error: `Runtime Error: Error: Module not found: Can't re

--- next.config.ts
+++ next.config.ts
@@ -43,6 +43,19 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+   webpack: (config, { isServer }) => {
+    // This is the correct way to prevent server-only modules from being bundled on the client.
+    if (!isServer) {
+      config.externals = [
+        ...config.externals,
+        // The following modules are dependencies of genkit's tracing system and are not needed on the client.
+        '@grpc/grpc-js', 
+        'node-pty'
+      ];
+    }
+
+    return config;
+  },
 };
 
 export default nextConfig;
