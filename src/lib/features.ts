7beaff296c021b97d14003bc9dd4751296fafff7
Bryan O'Callaghan <bryanocallaghan72@gmail.com>
2025-10-21 01:23:03 +0000

    try another image something different. as it doesn't work sorry.

--- src/lib/features.ts
+++ src/lib/features.ts
@@ -16,7 +16,7 @@ export const featureData = [
       title: "iykyk Code",
       description: "Meet the creators who are the source code of Bondi's vibe.",
       color: "text-indigo-500",
-      imageId: "ar-view",
+      imageId: "bondi-sunset",
     },
     {
       href: "/map",
