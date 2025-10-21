70a9d07d5becd5375526c59441fbb3a5f9707c9f
Bryan O'Callaghan <bryanocallaghan72@gmail.com>
2025-10-21 07:00:50 +0000

    Try fixing this error: `Build Error: Error: x Unexpected token `div`. Ex

--- src/components/iykyk/ar/ARPinLayer.tsx
+++ src/components/iykyk/ar/ARPinLayer.tsx
@@ -98,7 +98,7 @@ export function ARPinLayer({ activeLayer }: ARPinLayerProps) {
                                 </div>
                             </div>
                         </Link>
-                    </motion.motion.div>
+                    </motion.div>
                 ))}
             </AnimatePresence>
         </div>
