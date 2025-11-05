ba30d5282a002fc91397f494201bfd17f51157ed
Bryan O'Callaghan <bryanocallaghan72@gmail.com>
2025-11-05 01:55:11 +0000

    Application error: a client-side exception has occurred while loading 60

--- src/app/reels/page.tsx
+++ src/app/reels/page.tsx
@@ -1,14 +1,11 @@
 
 
 import { Reels } from "@/components/iykyk/Reels";
-import { FirebaseClientProvider } from "@/firebase/client-provider";
 
 export default function ReelsPage() {
   return (
-    <FirebaseClientProvider>
       <div className="bg-black h-screen w-full">
         <Reels />
       </div>
-    </FirebaseClientProvider>
   );
 }
