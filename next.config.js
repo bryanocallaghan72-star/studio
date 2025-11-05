c6a881e34c7fdb5631358ed0f0006e30159c8466
Bryan O'Callaghan <bryanocallaghan72@gmail.com>
2025-11-05 10:13:11 +0000

    ok lets work on this first then..Sorry i just need to prepare myself men

--- next.config.js
+++ next.config.js
@@ -27,6 +27,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'maps.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
    webpack: (config, { isServer }) => {
