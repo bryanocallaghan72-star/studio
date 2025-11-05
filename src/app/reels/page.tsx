df72fdfbaf0fe9bdc3a995ecd13a04663719231d
Bryan O'Callaghan <bryanocallaghan72@gmail.com>
2025-11-05 02:18:51 +0000

    Try fixing this error: `Runtime Error: useFirebase must be used within a

--- src/app/reels/page.tsx
+++ src/app/reels/page.tsx
@@ -1,11 +1,14 @@
 
 
 import { Reels } from "@/components/iykyk/Reels";
+import { FirebaseClientProvider } from "@/firebase/client-provider";
 
 export default function ReelsPage() {
   return (
-      <div className="bg-black h-screen w-full">
-        <Reels />
-      </div>
+      <FirebaseClientProvider>
+        <div className="bg-black h-screen w-full">
+          <Reels />
+        </div>
+      </FirebaseClientProvider>
   );
 }
