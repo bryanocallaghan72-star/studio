0d1c5e7f3a7be49ad5349d90599677cd2e4b4680
Bryan O'Callaghan <bryanocallaghan72@gmail.com>
2025-11-12 01:09:00 +0000

    please undo change to Header.

--- src/components/iykyk/Header.tsx
+++ src/components/iykyk/Header.tsx
@@ -33,7 +33,7 @@ export function Header() {
   }
 
   return (
-    <header className="flex h-14 items-center gap-4 bg-transparent px-4 lg:h-[60px] lg:px-6 sticky top-0 z-30">
+    <header className="flex h-14 items-center gap-4 border-b bg-transparent px-4 lg:h-[60px] lg:px-6 sticky top-0 z-30">
         <Sheet>
             <SheetTrigger asChild>
             <Button
