ae34270cb3caf2d8d7e33dd936d8a1067b9f23c9
Bryan O'Callaghan <bryanocallaghan72@gmail.com>
2025-11-13 00:10:29 +0000

    great idea but we shall come to this later..

--- src/app/reels/page.tsx
+++ src/app/reels/page.tsx
@@ -1,14 +1,12 @@
 
 
 import { Reels } from "@/components/iykyk/Reels";
-import { FirebaseClientProvider } from "@/firebase/client-provider";
 
 export default function ReelsPage() {
   return (
-      <FirebaseClientProvider>
-        <div className="bg-black h-screen w-full">
-          <Reels />
-        </div>
-      </FirebaseClientProvider>
+    <div className="h-screen w-full">
+      <Reels />
+    </div>
   );
 }
+
