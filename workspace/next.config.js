fc88ccaa789e55208107fdc3c62281cd589ebea4
Bryan O'Callaghan <bryanocallaghan72@gmail.com>
2025-11-26 13:04:40 +0000

    obviously the app hosting environment isn't working properly?

---
+++ workspace/next.config.js
@@ -0,0 +1,56 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'api.dicebear.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'maps.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+   webpack: (config, { isServer }) => {
+    // This is the correct way to prevent server-only modules from being bundled on the client.
+    if (!isServer) {
+      config.externals = [
+        ...(config.externals || []),
+        // The following modules are dependencies of genkit's tracing system and are not needed on the client.
+        '@grpc/grpc-js', 
+        'node-pty'
+      ];
+    }
+
+    return config;
+  },
+  env: {
+    NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
+  }
+};
+
+module.exports = nextConfig;
