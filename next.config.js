42e041ed2656559c20c80ccaf32421449701db5d
Bryan O'Callaghan <bryanocallaghan72@gmail.com>
2026-04-19 04:02:15 +0000

    ## Context FeedCard throws a runtime error because lh3.googleusercontent

--- next.config.js
+++ next.config.js
@@ -42,6 +42,14 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
       },
     ],
   },
